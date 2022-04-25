26a10bb240a9603e183530d3d98b82b0c091e0b1
Harsh Gupta <95281302+ElectroEnthu@users.noreply.github.com>
2022-04-25 19:52:06 +0530

    Create conf.py

---
+++ conf.py
@@ -0,0 +1,6 @@
+SID = 'sid num'
+AUTH_TOKEN = 'authentication token'
+FROM_NUMBER = '+twillio num'
+TO_NUMBER = '+91your phone number'
+API_KEY = 'your Bolt API id'
+DEVICE_ID = 'your Bolt device id'

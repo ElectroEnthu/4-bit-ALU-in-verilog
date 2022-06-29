418ab24dc52f947a7450063326b00d1ed69eab69
Harsh Gupta <95281302+ElectroEnthu@users.noreply.github.com>
2022-06-29 12:09:43 +0530

    Delete conf.py

--- conf.py
+++
@@ -1,6 +0,0 @@
-SID = 'sid num'
-AUTH_TOKEN = 'authentication token'
-FROM_NUMBER = '+twillio num'
-TO_NUMBER = '+91your phone number'
-API_KEY = 'your Bolt API id'
-DEVICE_ID = 'your Bolt device id'
